0531cd7e434db73a181d1663b1d6e5b80f5f86c7
Debayan Senapati <debsen@Debayans-MacBook-Air.local>
2025-01-11 01:17:24 +0530

    first commit

---
+++ 1.two-sum.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+from collections import defaultdict
+from typing import Counter
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = defaultdict(int)
+        for i, num in enumerate(nums):
+            d[num] = i
+
+        for i, num in enumerate(nums):
+            reminder = target - num
+            if reminder in d and i != d[reminder]:
+                return [i, d[reminder]]
+
+# @lc code=end
