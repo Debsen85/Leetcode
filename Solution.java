ca71188d2fa5ab8ae96d7c4dd3e188bbb4e90576
Debayan Senapati <debsen85@gmail.com>
2025-01-18 22:19:12 +0530

    BitManipulation commit

---
+++ Solution.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=190 lang=java
+ *
+ * [190] Reverse Bits
+ */
+
+// @lc code=start
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = (n >> i) & 1;
+            res += (bit << (31 - i));
+        }
+        return res;
+    }
+}
+// @lc code=end
+
