e1019a607b1bf9a908e697af965a6573863f045b
Debayan Senapati <debsen85@gmail.com>
2025-01-19 19:03:44 +0530

    Graph commit

---
+++ 207.course-schedule.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=207 lang=java
+ *
+ * [207] Course Schedule
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+    // Map each course to its prerequisites
+    private Map<Integer, List<Integer>> preMap = new HashMap<>();
+    // Store all courses along the current DFS path
+    private Set<Integer> visiting = new HashSet<>();
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        for (int i = 0; i < numCourses; i++) {
+            preMap.put(i, new ArrayList<>());
+        }
+        for (int[] prereq : prerequisites) {
+            preMap.get(prereq[0]).add(prereq[1]);
+        }
+
+        for (int c = 0; c < numCourses; c++) {
+            if (!dfs(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean dfs(int crs) {
+        if (visiting.contains(crs)) {
+            // Cycle detected
+            return false;
+        }
+        if (preMap.get(crs).isEmpty()) {
+            return true;
+        }
+
+        visiting.add(crs);
+        for (int pre : preMap.get(crs)) {
+            if (!dfs(pre)) {
+                return false;
+            }
+        }
+        visiting.remove(crs);
+        preMap.put(crs, new ArrayList<>());
+        return true;
+    }
+}
+// @lc code=end
+
