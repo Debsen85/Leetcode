063f7174171a2e655a61c790e35872c694825a11
Debayan Senapati <debsen85@gmail.com>
2025-02-20 16:05:40 +0530

    Leetcode Problem

--- 1043.partition-array-for-maximum-sum.py
+++ 1043.partition-array-for-maximum-sum.py
@@ -1,3 +1,4 @@
+
 #
 # @lc app=leetcode id=1043 lang=python3
 #
@@ -5,8 +6,22 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        
+        cache = {len(arr) : 0}
+        def backtracking(i):
+            if i in cache:
+                return cache[i]
+            currentMax = 0
+            result = 0
+            for j in range(i, min(i + k, len(arr))):
+                currentMax = max(currentMax, arr[j])
+                window = j - i + 1
+                result = max(result, backtracking(j + 1) + currentMax * window)
+            cache[i] = result
+            return result
+        return backtracking(0)
 # @lc code=end
 
