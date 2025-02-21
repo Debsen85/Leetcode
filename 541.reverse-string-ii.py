d43d0afe9e6ecdfde37528ddcf7e496ae8460228
Debayan Senapati <debsen85@gmail.com>
2025-02-21 15:41:50 +0530

    Leetcode Problem

--- 541.reverse-string-ii.py
+++ 541.reverse-string-ii.py
@@ -7,6 +7,16 @@
 # @lc code=start
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        
+        index = 0
+        s = list(s)
+        while index < len(s):
+            i = index
+            j = min(len(s), i + k) - 1
+            while i < j:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+            index += 2 * k
+        return "".join(s)
 # @lc code=end
 
