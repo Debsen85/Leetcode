4348912b89d391fad8f2c90104fd1bc2a5b8a735
Debayan Senapati <debsen85@gmail.com>
2025-02-21 09:17:45 +0530

    Leetcode Problem

--- 1770.maximum-score-from-performing-multiplication-operations.py
+++ 1770.maximum-score-from-performing-multiplication-operations.py
@@ -5,8 +5,20 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+        cache = [[-(10 ** 9)] * len(multipliers) for _ in range(len(nums))]
+        def backtracking(i, j, k):
+            if i > j or k == len(multipliers):
+                return 0
+            if cache[i][k] != -(10 ** 9):
+                return cache[i][k]
+            cache[i][k] = max(cache[i][k], max(nums[j] * multipliers[k] + backtracking(i, j - 1, k + 1), nums[i] * multipliers[k] + backtracking(i + 1, j, k + 1)))
+            return cache[i][k]
+        return backtracking(0, len(nums) - 1, 0)
         
 # @lc code=end
 
