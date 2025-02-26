ae9a0f1d7a05a4a9f494f920746d1e6303488e4c
Debayan Senapati <debsen85@gmail.com>
2025-02-26 19:50:12 +0530

    Leetcode Problem

---
+++ 2661.first-completely-painted-row-or-column.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=2661 lang=python3
+#
+# [2661] First Completely Painted Row or Column
+#
+
+# @lc code=start
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        arrayMap = {}
+        rows, cols = len(mat), len(mat[0])
+        for r in range(rows):
+            for c in range(cols):
+                arrayMap[mat[r][c]] = (r, c)
+
+        row = [0] * rows
+        col = [0] * cols
+
+        for i, ar in enumerate(arr):
+            r, c = arrayMap[ar]
+
+            row[r] += 1
+            col[c] += 1
+        
+            if row[r] == cols or col[c] == rows:
+                return i
+                
+        return -1
+# @lc code=end
+
