3adaf99c24786ca7791d0d97f1e5b45a8028ce2d
Debayan Senapati <debsen85@gmail.com>
2025-03-11 09:05:05 +0530

    Leetcode Problem

---
+++ 2337.move-pieces-to-obtain-a-string.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2337 lang=python3
+#
+# [2337] Move Pieces to Obtain a String
+#
+
+# @lc code=start
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        
+# @lc code=end
+
