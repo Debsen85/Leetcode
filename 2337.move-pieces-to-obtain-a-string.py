f730650d8c1e566672f6c16e4301f312c432f904
Debayan Senapati <debsen85@gmail.com>
2025-03-11 09:05:31 +0530

    Leetcode Problem

--- 2337.move-pieces-to-obtain-a-string.py
+++ 2337.move-pieces-to-obtain-a-string.py
@@ -7,6 +7,35 @@
 # @lc code=start
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
+        r = 0
+        j = len(target) - 1
+        while j > -1:
+            if target[j] == 'R':
+                r += 1
+            if start[j] == 'R':
+                if r > 0:
+                    r -= 1
+                else:
+                    return False
+            j -= 1
         
+        i = 0
+        l = 0
+        while i < len(target) - 1:
+            if target[i] == 'L':
+                l += 1
+            if start[i] == 'L':
+                if l > 0:
+                    l -= 1
+                else:
+                    return False
+            i += 1
+        
+        s, t = "", ""
+        for w1, w2 in zip(start, target):
+            s += w1 if w1 in 'LR' else ''
+            t += w2 if w2 in 'LR' else ''
+            
+        return s == t
 # @lc code=end
 
