6735e2ce2c4871d1fe632071966e8b2a38da5be8
Debayan Senapati <debsen85@gmail.com>
2025-03-16 18:13:24 +0530

    Leetcode Problem

--- 2594.minimum-time-to-repair-cars.py
+++ 2594.minimum-time-to-repair-cars.py
@@ -6,12 +6,14 @@
 
 # @lc code=start
 
+from typing import List
+
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
         def countOfCars(time):
             numberOfCars = 0
             for rank in ranks:
-                numberOfCars += int(sqrt(time // rank))
+                numberOfCars += int((time // rank) ** 0.5)
             return cars <= numberOfCars
 
         left, right, result = 1, max(ranks) * cars * cars, 1
