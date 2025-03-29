95d9760de697a5b1a61e32191a09c69c6ec4b0a3
Debayan Senapati <debsen85@gmail.com>
2025-03-29 21:08:29 +0530

    LeetCoding

--- 1014.best-sightseeing-pair.py
+++ 1014.best-sightseeing-pair.py
@@ -8,15 +8,11 @@
 from typing import List
 
 class Solution:
-    def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        n = len(values)
-        max_left_score = [0] * n
-        max_left_score[0] = values[0]
-        max_score = 0
-        for i in range(1, n):
-            current_right_score = values[i] - i
-            max_score = max(max_score, max_left_score[i - 1] + current_right_score)
-            current_left_score = values[i] + i
-            max_left_score[i] = max(max_left_score[i - 1], current_left_score)
-        return max_score
+    def maxScoreSightseeingPair(self, values):
+        result = 0
+        curr = values[0] - 1
+        for index in range(1, len(values)):
+            result = max(result, curr + values[index])
+            curr = max(values[index] - 1, curr - 1)
+        return result
 # @lc code=end
