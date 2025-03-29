eed87683db6ab5b496fb689b04e053f404ba4ff1
Debayan Senapati <debsen85@gmail.com>
2025-03-29 16:11:42 +0530

    LeetCoding

---
+++ 1014.best-sightseeing-pair.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1014 lang=python3
+#
+# [1014] Best Sightseeing Pair
+#
+
+# @lc code=start
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        
+# @lc code=end
+
