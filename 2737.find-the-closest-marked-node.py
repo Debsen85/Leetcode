47ececc6a399fb3b57f49fb8559c3f554b34380b
Debayan Senapati <debsen85@gmail.com>
2025-03-31 01:58:08 +0530

    LeetCoding

--- 2737.find-the-closest-marked-node.py
+++ 2737.find-the-closest-marked-node.py
@@ -5,43 +5,31 @@
 #
 
 # @lc code=start
-from cmath import inf
+from math import inf
 from collections import defaultdict
 import heapq
 from typing import List
 
 class Solution:
     def minimumDistance(self, n: int, edges: List[List[int]], s: int, marked: List[int]) -> int:
-        hs = defaultdict(set)
-
-        for i, j, v in edges:
-            hs[i].add((j, v))
-        
-        heap = []
-        heapq.heappush(heap, (0, s))
-        mn = inf
-        seen = defaultdict(int)
-
-        while heap:
-
-            w, n = heapq.heappop(heap)
-        
-            if n in marked:
-                mn = min(mn, w)
+        minHeap = []
+        result = {}
+        adjacencyList = defaultdict(list)
+        for u, v, w in edges:
+            adjacencyList[u].append((w, v))
+        heapq.heappush(minHeap, (0, s))
+        while minHeap:
+            currWeight, currNode = heapq.heappop(minHeap)
+            if currNode in result:
                 continue
-            
-            for nd, we in hs[n]:
-                if nd in seen:
-                    if seen[nd] < we + w:
-                        continue
-                    else:
-                        seen[nd] = we + w
-                else:
-                    seen[nd] = we + w
-                if nd in marked:
-                    mn = min(mn, we + w)
-                heapq.heappush(heap, (w + we, nd))
-
-        return mn if mn != inf else -1
+            result[currNode] = currWeight
+            for weight, node in adjacencyList[currNode]:
+                if node not in result:
+                    heapq.heappush(minHeap, (weight + currWeight, node))
+        answer = float("inf")
+        for mark in marked:
+            if mark in result:
+                answer = min(answer, result[mark])
+        return answer if answer != float("inf") else -1
 # @lc code=end
 
