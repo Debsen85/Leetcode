4152e5ba6230a635d70c20dd71c7a5dd0e638870
Debayan Senapati <debsen85@gmail.com>
2025-04-14 02:16:22 +0530

    JavaProgramming

--- 1922.count-good-numbers.py
+++ 1922.count-good-numbers.py
@@ -7,6 +7,10 @@
 # @lc code=start
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        
+        h = n // 2
+        m = 10 ** 9 + 7
+        if n == 1: return 5
+        if n % 2: return (pow(5, h, m) * pow(4, h, m) * 5) % m
+        return (pow(5, h, m) * pow(4, h, m)) % m
 # @lc code=end
 
