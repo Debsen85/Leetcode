8ef7267d773c99e6a4cde39288442bc6dc4bd337
Debayan Senapati <debsen85@gmail.com>
2025-04-14 01:53:02 +0530

    JavaProgramming

---
+++ 1922.count-good-numbers.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1922 lang=python3
+#
+# [1922] Count Good Numbers
+#
+
+# @lc code=start
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        
+# @lc code=end
+
